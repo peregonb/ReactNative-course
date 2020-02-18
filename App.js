24d38f14af7566bcfd3d83af6433028391245005
Peregon <peregonbw@gmail.com>
2020-02-18 14:26:06 +0200

    add todo

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Navbar} from "./src/Navbar";
+import {AddTodo} from "./src/AddTodo";
+import {Todo} from "./src/Todo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
-  );
+export default () => {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = title => {
+        // const newTodo = {
+        //     id: Date.now().toString(),
+        //     title: title
+        // };
+        // setTodos(todos.concat([newTodo])) мутирующа
+        // setTodos((prevTodos) => {
+        //     return [
+        //         ...prevTodos,
+        //         newTodo
+        //     ]
+        // })
+
+        setTodos(prev => [...prev,
+            {
+                id: Date.now().toString(),
+                title
+            }
+        ])
+    };
+
+    return (
+        <View style={styles.container}>
+            <Navbar/>
+            <View style={styles.wrapper}>
+                <AddTodo onSubmit={addTodo}/>
+
+                <View>
+                    {todos.map(todo => <Todo key={todo.id} todo={todo}/>)}
+                </View>
+            </View>
+        </View>
+    )
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {},
+    wrapper: {
+        paddingHorizontal: 20,
+        paddingVertical: 10
+    },
 });
